c744ecf3a9277e4840ab688ef002701d2551f29f
Laurie2905-JOHN <127870295+Laurie2905-JOHN@users.noreply.github.com>
2023-03-17 22:27:24 +0000

    Initial Test 1

--- main.py
+++ main.py
@@ -10,15 +10,34 @@ VectFile = 'MinMon20210607150154'
 
 from scipy.io import loadmat
 Cal1 = loadmat(FilePath+CalFile)
-Cal=(Cal1["Cal"])
-print(Cal)
+Cal2=Cal1["Cal"]
+
+Dynfit = (Cal2[0][0][0])
+Yawfit = (Cal2[0][0][1])
+LDyn = (Cal2[0][0][2])
+LYaw = (Cal2[0][0][3])
+LDyn_0 = (Cal2[0][0][4])
 
 import numpy as np
 
-print(len(Cal))
-
 from numpy import linspace
 yawcal=linspace(-45,45,91)
+
+
+# Define a polynomial with coefficients [2, 3, 1]
+p = np.array([2, 3, 1])
+
+# Evaluate the polynomial at x = 2
+x = 2
+y = np.polyval(p, x)
+
+print(y) # Output: 15
+
+
+import numpy as np
+
+
+
 
 
 
