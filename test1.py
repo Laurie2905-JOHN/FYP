ad1a6c34e17dd4b5514d688a894de0b51fbe731c
Laurie2905-JOHN <127870295+Laurie2905-JOHN@users.noreply.github.com>
2023-03-27 10:53:07 +0100

    testing tkinter gui. Workign and selects files trying to import

--- test1.py
+++ test1.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter.filedialog as fd
+from pathlib import Path, PureWindowsPath
 
 
 
@@ -22,7 +23,9 @@ def file_chooser():
     def open_file():
         files = fd.askopenfilenames(parent=win, title='Choose a File')
         global file_paths
-        file_paths = win.splitlist(files)
+        file_paths = list(win.splitlist(files))
+        for file_path in file_paths:
+            file_paths.append(Path(file_path))
         # Clear the Treeview widget before inserting new file names
         tree.delete(*tree.get_children())
         # Update the table with the selected file names
@@ -40,6 +43,7 @@ def file_chooser():
 
     def close_window():
         win.destroy()
+
     # Add a Label widget for close button
     label = Label(win, text="Close window once files are added", font=('Aerial 11'))
     label.pack(pady=30)
@@ -47,89 +51,97 @@ def file_chooser():
 
     win.mainloop()
 
+def cal_velocity(BarnFilePath):
+    import numpy as np
+    import scipy.io as sio
+
+    # Constants
+    rho = 997
+    fs = 16  # sample rate
+
+    # File retrieving
+    ZeroFolder = "C:/Users/lauri/OneDrive/Documents (1)/University/Year 3/Semester 2/BARNACLE/Example Data/"
+    ZeroFile = 'Mon1527.txt'
+    CalFolder = ZeroFolder
+    CalFile = 'IanYawAndDynCalMk2.mat'
+    Cal = sio.loadmat(CalFolder + CalFile)
+
+    Cal = Cal["Cal"]
+    Dynfit = Cal[0][0][0].flatten()
+    Yawfit = Cal[0][0][1].flatten()
+    LDyn = Cal[0][0][2].flatten()
+    LYaw = Cal[0][0][3].flatten()
+    LDyn_0 = Cal[0][0][4].flatten()
+
+    # Evaluating yawcal for a polynomial Cal.Yawfit and dyncal
+    yawcal = np.zeros((91, 2))
+    yawcal[:, 0] = np.linspace(-45, 45, 91)
+    yawcal[:, 1] = np.polyval(Yawfit, yawcal[:, 0])
+    dyncal = np.polyval(Dynfit, yawcal[:, 0])
+    dyncal = dyncal * LDyn_0
+
+    # Importing Zeroes
+    zeros = {}
+    zeros['pr_raw'] = np.loadtxt(ZeroFolder + ZeroFile, delimiter=',')
+    zeros['pr_mean'] = np.mean(zeros['pr_raw'][1300:1708, :], axis=0)
+
+    # Loading actual Barnacle data
+    prb = {}
+    prb['raw'] = np.loadtxt(BarnFilePath, delimiter=',')
+    prb['raw'] -= zeros['pr_mean']
+
+    # Data analysis
+    prb['denom'] = np.mean(prb['raw'][:, :4], axis=1)
+    prb['Lyaw'] = (prb['raw'][:, 1] - prb['raw'][:, 3]) / prb['denom']
+    prb['Lpitch'] = (prb['raw'][:, 0] - prb['raw'][:, 2]) / prb['denom']
+
+    from scipy import interpolate
+
+    ayaw_interp = interpolate.interp1d(yawcal[:, 1], yawcal[:, 0], kind='linear', fill_value='extrapolate')
+    apitch_interp = interpolate.interp1d(yawcal[:, 1], yawcal[:, 0], kind='linear', fill_value='extrapolate')
+    prb['ayaw'] = ayaw_interp(prb['Lyaw'])
+    prb['apitch'] = apitch_interp(prb['Lpitch'])
+    prb['pitchbigger'] = np.abs(prb['apitch']) > np.abs(prb['ayaw'])
+    prb['amax'] = prb['pitchbigger'] * prb['apitch'] + (1 - prb['pitchbigger']) * prb['ayaw']
+    ldyn_interp = interpolate.interp1d(yawcal[:, 0], dyncal, kind='linear', fill_value='extrapolate')
+    prb['ldyn'] = ldyn_interp(prb['amax'])
+
+    # Splitting into velocities
+    prb['U1'] = np.sqrt(2 * -prb['ldyn'] * np.mean(prb['raw'][:, :4], axis=1) / rho)
+    prb['U1'][np.imag(prb['U1']) > 0] = 0
+    prb['Ux'] = prb['U1'] * np.cos(np.deg2rad(prb['apitch'])) * np.cos(np.deg2rad(prb['ayaw']))
+    prb['Uy'] = prb['U1'] * np.cos(np.deg2rad(prb['apitch'])) * np.sin(np.deg2rad(prb['ayaw']))
+    prb['Uz'] = prb['U1'] * np.sin(np.deg2rad(prb['apitch']))
+    prb['t'] = np.linspace(0, prb['raw'].shape[0] / fs, prb['raw'].shape[0]);
+
+    return prb
+
+
+
 
 file_chooser()
 
 print(file_names)
+#print(file_paths)
+
+
+
+
 print(file_paths)
 
 
+BarnFilePath = file_paths
 
-# def cal_velocity(BarnFilePath):
-#     import numpy as np
-#     import scipy.io as sio
-#
-#     # Constants
-#     rho = 997
-#     fs = 16  # sample rate
-#
-#     # File retrieving
-#     ZeroFolder = "C:/Users/lauri/OneDrive/Documents (1)/University/Year 3/Semester 2/BARNACLE/Example Data/"
-#     ZeroFile = 'Mon1527.txt'
-#     CalFolder = ZeroFolder
-#     CalFile = 'IanYawAndDynCalMk2.mat'
-#     Cal = sio.loadmat(CalFolder + CalFile)
-#
-#     Cal = Cal["Cal"]
-#     Dynfit = Cal[0][0][0].flatten()
-#     Yawfit = Cal[0][0][1].flatten()
-#     LDyn = Cal[0][0][2].flatten()
-#     LYaw = Cal[0][0][3].flatten()
-#     LDyn_0 = Cal[0][0][4].flatten()
-#
-#     # Evaluating yawcal for a polynomial Cal.Yawfit and dyncal
-#     yawcal = np.zeros((91, 2))
-#     yawcal[:, 0] = np.linspace(-45, 45, 91)
-#     yawcal[:, 1] = np.polyval(Yawfit, yawcal[:, 0])
-#     dyncal = np.polyval(Dynfit, yawcal[:, 0])
-#     dyncal = dyncal * LDyn_0
-#
-#     # Importing Zeroes
-#     zeros = {}
-#     zeros['pr_raw'] = np.loadtxt(ZeroFolder + ZeroFile, delimiter=',')
-#     zeros['pr_mean'] = np.mean(zeros['pr_raw'][1300:1708, :], axis=0)
-#
-#     # Loading actual Barnacle data
-#     prb = {}
-#     prb['raw'] = np.loadtxt(BarnFilePath, delimiter=',')
-#     prb['raw'] -= zeros['pr_mean']
-#
-#     # Data analysis
-#     prb['denom'] = np.mean(prb['raw'][:, :4], axis=1)
-#     prb['Lyaw'] = (prb['raw'][:, 1] - prb['raw'][:, 3]) / prb['denom']
-#     prb['Lpitch'] = (prb['raw'][:, 0] - prb['raw'][:, 2]) / prb['denom']
-#
-#     from scipy import interpolate
-#
-#     ayaw_interp = interpolate.interp1d(yawcal[:, 1], yawcal[:, 0], kind='linear', fill_value='extrapolate')
-#     apitch_interp = interpolate.interp1d(yawcal[:, 1], yawcal[:, 0], kind='linear', fill_value='extrapolate')
-#     prb['ayaw'] = ayaw_interp(prb['Lyaw'])
-#     prb['apitch'] = apitch_interp(prb['Lpitch'])
-#     prb['pitchbigger'] = np.abs(prb['apitch']) > np.abs(prb['ayaw'])
-#     prb['amax'] = prb['pitchbigger'] * prb['apitch'] + (1 - prb['pitchbigger']) * prb['ayaw']
-#     ldyn_interp = interpolate.interp1d(yawcal[:, 0], dyncal, kind='linear', fill_value='extrapolate')
-#     prb['ldyn'] = ldyn_interp(prb['amax'])
-#
-#     # Splitting into velocities
-#     prb['U1'] = np.sqrt(2 * -prb['ldyn'] * np.mean(prb['raw'][:, :4], axis=1) / rho)
-#     prb['U1'][np.imag(prb['U1']) > 0] = 0
-#     prb['Ux'] = prb['U1'] * np.cos(np.deg2rad(prb['apitch'])) * np.cos(np.deg2rad(prb['ayaw']))
-#     prb['Uy'] = prb['U1'] * np.cos(np.deg2rad(prb['apitch'])) * np.sin(np.deg2rad(prb['ayaw']))
-#     prb['Uz'] = prb['U1'] * np.sin(np.deg2rad(prb['apitch']))
-#     prb['t'] = np.linspace(0, prb['raw'].shape[0] / fs, prb['raw'].shape[0]);
-#
-#     return prb
-#
-#     cal_velocity(File_Paths)
-#
-# file_chooser()
-# prb = []
-# df = {}
+#=cal_velocity(file_paths)
+
+prb = []
+df = {}
 # for file_path in selected_files:
 #     prb1 = cal_velocity(file_path)
 #     prb.append(prb1)
 #     file_name = os.path.basename(file_path)
 #     df[file_name] = prb
+
 
 
 
