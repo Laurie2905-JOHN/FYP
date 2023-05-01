a3fc85a03009b89d2db0da555771f8a30e52cfed
Laurie2905-JOHN <127870295+Laurie2905-JOHN@users.noreply.github.com>
2023-05-01 14:06:33 +0100

    install try again

--- create_executable.py
+++ create_executable.py
@@ -1,20 +1,45 @@
 import os
 import subprocess
 
+
 def main():
     requirements_file = 'requirements.txt'
+    main_script = 'dash_app.py'
 
+    # Read packages from requirements.txt
     with open(requirements_file, 'r') as f:
         lines = f.readlines()
-
     packages = [line.strip().split('==')[0] for line in lines if line.strip()]
 
-    hidden_imports = ' '.join([f'--hidden-import={package}' for package in packages])
+    # Create a spec file
+    subprocess.run(f'pyi-makespec --onefile {main_script}', shell=True)
 
-    pyinstaller_command = f'pyinstaller --onefile {hidden_imports} dash_app.py'
+    spec_file = main_script.replace('.py', '.spec')
 
-    print(f'Running: {pyinstaller_command}')
-    subprocess.run(pyinstaller_command, shell=True)
+    # Read spec file
+    with open(spec_file, 'r') as f:
+        spec_content = f.readlines()
+
+    # Find the 'datas' line in the spec file
+    for i, line in enumerate(spec_content):
+        if 'datas=' in line:
+            break
+
+    # Prepare the datas list for packages
+    venv_path = os.path.join('venv', 'Lib', 'site-packages')
+    datas = [f"('{os.path.join(venv_path, package)}', '{package}')" for package in packages]
+    datas_line = f"datas=[{', '.join(datas)}],\n"
+
+    # Update the 'datas' line in the spec file
+    spec_content[i] = datas_line
+
+    # Write the updated spec file
+    with open(spec_file, 'w') as f:
+        f.writelines(spec_content)
+
+    # Run PyInstaller with the updated spec file
+    subprocess.run(f'pyinstaller {spec_file}', shell=True)
+
 
 if __name__ == '__main__':
     main()
